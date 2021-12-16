19eefa4e868c249ab80c293982b5c1c326abdf4b
madhavrjha-lab <madhav@nextnow.africa>
2021-12-16 20:10:28 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// Sticky Header
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
