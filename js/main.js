36e8cb2f24f1419fc005cbe0c414a55f00d7d89e
madhavrjha-lab <madhav@nextnow.africa>
2021-12-28 13:12:03 +0530

    Sticky Part Removed

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 
 // Sticky Header
 
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
-});
+// window.addEventListener('scroll', function() {
+//   const header = document.querySelector('header');
+//   header.classList.toggle('sticky', window.scrollY > 0);
+// });
 
 // Hover on Header
 
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 
-header.addEventListener('mouseenter', () => {
-  console.log('Mouse Entered');
-  if(window.scrollY == 0) header.classList.add('sticky');
-});
+// header.addEventListener('mouseenter', () => {
+//   console.log('Mouse Entered');
+//   if(window.scrollY == 0) header.classList.add('sticky');
+// });
 
-header.addEventListener('mouseleave', () => {
-  console.log('Mouse Leave');
-  if(window.scrollY == 0) header.classList.remove('sticky');
-});
+// header.addEventListener('mouseleave', () => {
+//   console.log('Mouse Leave');
+//   if(window.scrollY == 0) header.classList.remove('sticky');
+// });
 
 
 // Click on Bars and Times
