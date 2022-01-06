0024acf6b176d4c12e41833d73dd6b7833cc11e7
madhavrjha-lab <madhav@nextnow.africa>
2022-01-06 15:31:35 +0530

    Side Menu Added Tet

--- js/main.js
+++ js/main.js
@@ -1,30 +1,7 @@
-
-// Sticky Header
-
-// window.addEventListener('scroll', function() {
-//   const header = document.querySelector('header');
-//   header.classList.toggle('sticky', window.scrollY > 0);
-// });
-
-// Hover on Header
-
-// const header = document.querySelector('header');
-
-// header.addEventListener('mouseenter', () => {
-//   console.log('Mouse Entered');
-//   if(window.scrollY == 0) header.classList.add('sticky');
-// });
-
-// header.addEventListener('mouseleave', () => {
-//   console.log('Mouse Leave');
-//   if(window.scrollY == 0) header.classList.remove('sticky');
-// });
-
-
 // Click on Bars and Times
 
-const headerBars = document.querySelector('header .container > i');
-const headerTimes = document.querySelector('aside .container > i');
+const headerBars = document.querySelector('header .container > i.fas.fa-bars');
+const headerTimes = document.querySelector('aside .close-btn');
 const aside = document.querySelector('aside');
 
 headerBars.addEventListener('click', () => {
