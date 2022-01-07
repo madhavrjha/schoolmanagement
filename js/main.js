86723c6ef9355994b1c04531ba45958a209053dd
madhavrjha-lab <madhav@nextnow.africa>
2022-01-07 12:43:32 +0530

    Swipe Left and Swipe Right Event Listener Added To Collapse and Expand the Aside menu

--- js/main.js
+++ js/main.js
@@ -28,6 +28,65 @@ $(document).ready(function () {
 
 
 
+/* 
+    JavaScript for 
+     - Collapse and Expand the Menu in Mobile Devices By Swiping Left and Right
+     - Collapse and Expand the Header in Mobile Devices By wiping Up and Down
+*/
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            // Right Swipe
+            if(aside.classList.contains('active')) 
+              aside.classList.remove('active');
+        } else {
+          // Left Swipe
+          if(!aside.classList.contains('active')) 
+            aside.classList.add('active');
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+          // Down Swipe
+          // $('header').slideUp();
+        } else { 
+          // Up Swipe
+          // $('header').slideDown();
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 // Animate On Scroll Initialization
 
 AOS.init({
